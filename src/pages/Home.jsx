4f734bb78d309b9022d848f2dbe81c7278d92669
Abdulwahab Sani <abdulwsani@gmail.com>
2025-10-26 14:25:21 +0100

    Import Swiper and React Icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,56 @@
 import React from "react";
 import styles from "./Home.module.css";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FaArrowRight } from "react-icons/fa";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Pagination, Navigation } from "swiper/modules";
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <main className={styles.home}>
-      <h1>Welcome to Hi-Grade Systems Limited</h1>
-    </main>
+    <>
+      {/* main section on homepage */}
+      <section className={styles.home}>
+        <div className="home-overlay">
+          <h1>Welcome to Hi-Grade Systems Ltd</h1>
+          <p>
+            Your one-stop destination for graded quality real estate
+            construction & fiscal management consultancy.
+          </p>
+          <Link to="/about">
+            <button className="btn-home">
+              <div className="home-about">About Us</div>
+              <div>
+                <FaArrowRight />
+              </div>
+            </button>
+          </Link>
+        </div>
+
+        {/* carousel slider */}
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          pagination={{ clickable: true }}
+          navigation={true}
+          loop={true}
+          modules={[Pagination, Navigation]}
+          className={styles.homeslider}
+        >
+          <SwiperSlide>
+            <img src="/images/homepage-cover.JPG" alt="Slide 1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/images/background-cover.jpg" alt="Slide 2" />
+          </SwiperSlide>
+        </Swiper>
+      </section>
+    </>
   );
 }
 
