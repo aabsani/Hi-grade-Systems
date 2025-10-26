9d249de01af87ed246dce72fcc8895faa77059cf
Abdulwahab Sani <abdulwsani@gmail.com>
2025-10-26 14:51:37 +0100

    Create Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,63 @@
+import styles from "./Navbar.module.css";
 
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { FaFacebookF, FaInstagram, FaXTwitter } from "react-icons/fa6";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: '#0a5825', padding: '10px' }}>
-      <Link to="/" style={{ margin: '0 15px', color: 'white' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 15px', color: 'white' }}>About</Link>
-      <Link to="/services" style={{ margin: '0 15px', color: 'white' }}>Services</Link>
-      <Link to="/projects" style={{ margin: '0 15px', color: 'white' }}>Projects</Link>
-      <Link to="/contact" style={{ margin: '0 15px', color: 'white' }}>Contact</Link>
+    <nav className={styles.navbar}>
+      <NavLink to="/">
+        <img src="/images/logo-1.jpeg" alt="logo" className="logo" />
+      </NavLink>
+
+      <ul>
+        <li>
+          <NavLink to="/" className="nav-item">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className="nav-item">
+            About Us
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/services" className="nav-item">
+            Services
+          </NavLink>
+        </li>
+        {/* <li>
+          <NavLink to="/gallery" className="nav-item">
+            Gallery
+          </NavLink>
+        </li> */}
+        <li>
+          <NavLink to="/contact" className="nav-item">
+            Contact Us
+          </NavLink>
+        </li>
+      </ul>
+
+      <div className="social-icons">
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebookF size={12} />
+        </a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram size={12} />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <FaXTwitter size={12} />
+        </a>
+      </div>
     </nav>
   );
 }
